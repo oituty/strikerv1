3fd3102ea3a8e3c9bb5cc9e8a8e8ed398b9f44bb
Tayna Alves <taynaalro@gmail.com>
2018-04-17 20:51:30 -0300

    cadastro

---
+++ cadastro.js
@@ -0,0 +1,9 @@
+function validarSenha(){
+	senha1 = document.f1.senha1.value
+	senha2 = document.f1.senha2.value
+ 
+	if (senha1 == senha2)
+		alert("SENHAS IGUAIS")
+	else
+		alert("SENHAS DIFERENTES")
+}
